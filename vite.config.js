272317aa863aa055d29980188a1ebe3031ba3b20
MohamedAdeL6 <mohamedadel.ma.098@gmail.com>
2025-11-08 07:54:48 +0200

    Edite vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/E-commerce_Website_React_Vite/",
+  base: "/E-commerce_Website_React_Vite",
 })
